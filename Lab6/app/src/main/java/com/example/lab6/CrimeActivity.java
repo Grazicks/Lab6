4d78952f165c9de84f99d862653077fb56ceba39
Grazicks <rodion.tarasov.03@mail.ru>
2023-12-26 00:23:07 +0700

    Lab6

---
+++ Lab6/app/src/main/java/com/example/lab6/CrimeActivity.java
@@ -0,0 +1,10 @@
+package com.example.lab6;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
